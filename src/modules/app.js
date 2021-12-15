13483ff69ba4496fcaceae77a38d6fe309e158a8
Yuriy Koziy <yukoziy@gmail.com>
2021-12-15 18:56:39 -0500

    Added test function, to call api functions....

--- src/modules/app.js
+++ src/modules/app.js
@@ -1,5 +1,13 @@
+import * as api from './apiFunctions.js';
+
+async function test() {
+  let z = await api.fetchWeather('Pittsburgh');
+  console.log(z);
+}
+
 function init() {
-    console.log("Hello world");
+  console.log('Hello world');
+  test();
 }
 
 export { init };
