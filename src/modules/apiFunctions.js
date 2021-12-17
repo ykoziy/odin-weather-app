8258f690ab201728b5aad4ef0796b945e680bfa1
Yuriy Koziy <yukoziy@gmail.com>
2021-12-17 12:02:15 -0500

    Added function to build weather request URL, refactored fetchWeather

--- src/modules/apiFunctions.js
+++ src/modules/apiFunctions.js
@@ -1,12 +1,13 @@
 const API_KEY = 'bee7597dcba3a8d3b0345cbdbb7cc8ca';
 
-async function fetchWeather(cityName) {
-  const response = await fetch(
-    `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`,
-    { mode: 'cors' },
-  );
+function currentWeatherUrl(cityName) {
+  return `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}`;
+}
+
+async function fetchWeather(url) {
+  const response = await fetch(url, { mode: 'cors' });
   const data = await response.json();
   return data;
 }
 
-export { fetchWeather };
+export { currentWeatherUrl, fetchWeather };
