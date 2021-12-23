a67725db1401ca1cd9f8f47218e3ef42b5ec978c
Yuriy Koziy <yukoziy@gmail.com>
2021-12-23 12:50:01 -0500

    Refactor, added RenderWeather class.

--- src/modules/dom/currentWeather.js
+++ src/modules/dom/currentWeather.js
@@ -11,82 +11,87 @@ function getDay(dayNum) {
   return days[dayNum];
 }
 
-function renderMainWeather(parentElement, weatherData) {
-  let cityNameHeading = document.createElement('h1');
-  cityNameHeading.innerHTML = weatherData.city;
-  parentElement.appendChild(cityNameHeading);
+class RenderWeather {
+  constructor(weatherData, containerClass, isMetric = false) {
+    this.weatherData = weatherData;
+    this.isMetric = isMetric;
+    this.container = document.querySelector(containerClass);
+  }
 
-  let asOfHeading = document.createElement('h2');
-  //unix time in api is seconds...
-  //JS date is always local time.....
-  let date = new Date(weatherData.dt * 1000);
-  let day = getDay(date.getDay());
-  asOfHeading.innerHTML = `${day} and 11:11 PM`;
+  #renderMainWeather(parentElement, weatherData) {
+    let cityNameHeading = document.createElement('h1');
+    cityNameHeading.innerHTML = weatherData.city;
+    parentElement.appendChild(cityNameHeading);
 
-  parentElement.appendChild(asOfHeading);
+    let asOfHeading = document.createElement('h2');
+    //unix time in api is seconds...
+    //JS date is always local time.....
+    let date = new Date(weatherData.dt * 1000);
+    let day = getDay(date.getDay());
+    asOfHeading.innerHTML = `${day} and 11:11 PM`;
 
-  let tempPar = document.createElement('p');
-  tempPar.innerHTML = `${weatherData.temp} K`;
-  parentElement.appendChild(tempPar);
+    parentElement.appendChild(asOfHeading);
 
-  let conditionsPar = document.createElement('p');
-  conditionsPar.innerHTML = weatherData.conditions;
-  parentElement.appendChild(conditionsPar);
+    let tempPar = document.createElement('p');
+    tempPar.innerHTML = `${weatherData.temp} K`;
+    parentElement.appendChild(tempPar);
+
+    let conditionsPar = document.createElement('p');
+    conditionsPar.innerHTML = weatherData.conditions;
+    parentElement.appendChild(conditionsPar);
+  }
+
+  #renderOtherConditions(parentElement, weatherData) {
+    let feelsLikePar = document.createElement('p');
+    feelsLikePar.innerHTML = `Feels like: ${weatherData.tempFeelsLike} K`;
+    parentElement.appendChild(feelsLikePar);
+
+    let humidityPar = document.createElement('p');
+    humidityPar.innerHTML = `Humidity: ${weatherData.humidity}%`;
+    parentElement.appendChild(humidityPar);
+
+    let pressurePar = document.createElement('p');
+    pressurePar.innerHTML = `Pressure: ${weatherData.pressure} hPa`;
+    parentElement.appendChild(pressurePar);
+
+    let visibilityPar = document.createElement('p');
+    visibilityPar.innerHTML = `Visibility: ${weatherData.visibility} m`;
+    parentElement.appendChild(visibilityPar);
+
+    let windPar = document.createElement('p');
+    windPar.innerHTML = `Wind ${weatherData.windDir} at ${weatherData.windSpeed} m/s`;
+    parentElement.appendChild(windPar);
+  }
+
+  render() {
+    let mainWeatherDiv = document.createElement('div');
+    mainWeatherDiv.className = 'current-weather-left';
+
+    let mainWeatherData = {
+      city: this.weatherData.city,
+      dt: this.weatherData.dt,
+      timezone: this.weatherData.timezone,
+      temp: this.weatherData.weather.temp,
+      conditions: this.weatherData.weather.conditions,
+    };
+    this.#renderMainWeather(mainWeatherDiv, mainWeatherData);
+
+    let otherConditionsDiv = document.createElement('div');
+    otherConditionsDiv.className = 'current-weather-right';
+
+    let otherWeatherData = {
+      humidity: this.weatherData.weather.humidity,
+      tempFeelsLike: this.weatherData.weather.tempFeelsLike,
+      pressure: this.weatherData.weather.pressure,
+      windDir: this.weatherData.weather.windDir,
+      windSpeed: this.weatherData.weather.windSpeed,
+      visibility: this.weatherData.weather.visibility,
+    };
+    this.#renderOtherConditions(otherConditionsDiv, otherWeatherData);
+
+    this.container.appendChild(mainWeatherDiv);
+    this.container.appendChild(otherConditionsDiv);
+  }
 }
 
-function renderOtherConditions(parentElement, weatherData) {
-  let feelsLikePar = document.createElement('p');
-  feelsLikePar.innerHTML = `Feels like: ${weatherData.tempFeelsLike} K`;
-  parentElement.appendChild(feelsLikePar);
-
-  let humidityPar = document.createElement('p');
-  humidityPar.innerHTML = `Humidity: ${weatherData.humidity}%`;
-  parentElement.appendChild(humidityPar);
-
-  let pressurePar = document.createElement('p');
-  pressurePar.innerHTML = `Pressure: ${weatherData.pressure} hPa`;
-  parentElement.appendChild(pressurePar);
-
-  let visibilityPar = document.createElement('p');
-  visibilityPar.innerHTML = `Visibility: ${weatherData.visibility} m`;
-  parentElement.appendChild(visibilityPar);
-
-  let windPar = document.createElement('p');
-  windPar.innerHTML = `Wind ${weatherData.windDir} at ${weatherData.windSpeed} m/s`;
-  parentElement.appendChild(windPar);
-}
-
-function renderCurrentWeather(weatherData) {
-  const container = document.querySelector('.weather-container');
-
-  let mainWeatherDiv = document.createElement('div');
-  mainWeatherDiv.className = 'current-weather-left';
-
-  let mainWeatherData = {
-    city: weatherData.city,
-    dt: weatherData.dt,
-    timezone: weatherData.timezone,
-    temp: weatherData.weather.temp,
-    conditions: weatherData.weather.conditions,
-  };
-  renderMainWeather(mainWeatherDiv, mainWeatherData);
-
-  let otherConditionsDiv = document.createElement('div');
-  otherConditionsDiv.className = 'current-weather-right';
-
-  let otherWeatherData = {
-    humidity: weatherData.weather.humidity,
-    tempFeelsLike: weatherData.weather.tempFeelsLike,
-    pressure: weatherData.weather.pressure,
-    windDir: weatherData.weather.windDir,
-    windSpeed: weatherData.weather.windSpeed,
-    visibility: weatherData.weather.visibility,
-  };
-  renderOtherConditions(otherConditionsDiv, otherWeatherData);
-
-  container.appendChild(mainWeatherDiv);
-  container.appendChild(otherConditionsDiv);
-  console.log(weatherData.city);
-}
-
-export { renderCurrentWeather };
+export { RenderWeather };
