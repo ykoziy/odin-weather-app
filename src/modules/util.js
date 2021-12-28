9b062f2b2faa6175e7bdde2694456de8eb2ada9d
Yuriy Koziy <yukoziy@gmail.com>
2021-12-28 14:38:06 -0500

    Added getWeatherIcon util function.

--- src/modules/util.js
+++ src/modules/util.js
@@ -83,4 +83,30 @@ function capitalizeWords(str) {
     .join(' ');
 }
 
-export { UnitConversion, capitalizeWords };
+function getWeatherIcon(weatherConditions) {
+  const weatherIcons = {
+    clear: 'img/icons/clear-w.svg',
+    clouds: 'img/icons/clouds-w.svg',
+    rain: 'img/icons/rain-w.svg',
+    snow: 'img/icons/snow-w.svg',
+    thunder: 'img/icons/thunder-w.svg',
+  };
+
+  const body = document.getElementsByTagName('body')[0];
+  if (weatherConditions.includes('clear')) {
+    return weatherIcons.clear;
+  } else if (weatherConditions.includes('clouds')) {
+    return weatherIcons.clouds;
+  } else if (
+    weatherConditions.includes('rain') ||
+    weatherConditions.includes('drizzle')
+  ) {
+    return weatherIcons.rain;
+  } else if (weatherConditions.includes('snow')) {
+    return weatherIcons.snow;
+  } else if (weatherConditions.includes('thunder')) {
+    return weatherIcons.thunder;
+  }
+}
+
+export { UnitConversion, capitalizeWords, getWeatherIcon };
