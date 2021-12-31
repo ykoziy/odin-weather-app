4b842941b13739583110f08d822e805f4b0df59d
Yuriy Koziy <yukoziy@gmail.com>
2021-12-31 17:10:25 -0500

    removed dom functions from normal util module.

--- src/modules/util.js
+++ src/modules/util.js
@@ -83,39 +83,4 @@ function capitalizeWords(str) {
     .join(' ');
 }
 
-function getWeatherIcon(weatherConditions) {
-  const weatherIcons = {
-    clear: 'img/icons/clear-w.svg',
-    clouds: 'img/icons/clouds-w.svg',
-    rain: 'img/icons/rain-w.svg',
-    snow: 'img/icons/snow-w.svg',
-    thunder: 'img/icons/thunder-w.svg',
-  };
-
-  const body = document.getElementsByTagName('body')[0];
-  if (weatherConditions.includes('clear')) {
-    return weatherIcons.clear;
-  } else if (weatherConditions.includes('clouds')) {
-    return weatherIcons.clouds;
-  } else if (
-    weatherConditions.includes('rain') ||
-    weatherConditions.includes('drizzle')
-  ) {
-    return weatherIcons.rain;
-  } else if (weatherConditions.includes('snow')) {
-    return weatherIcons.snow;
-  } else if (weatherConditions.includes('thunder')) {
-    return weatherIcons.thunder;
-  }
-}
-
-function toggleActiveButton(buttons, target) {
-  buttons.forEach((button) => {
-    if (button === target && !button.classList.contains('active-btn')) {
-      return button.classList.add('active-btn');
-    }
-    return button.classList.remove('active-btn');
-  });
-}
-
-export { UnitConversion, capitalizeWords, getWeatherIcon, toggleActiveButton };
+export { UnitConversion, capitalizeWords };
